ffed47f3f299974f305b0c3852613030069a9200
Nicolas_Leonidas <yasser1984ir@gmail.com>
2013-11-06 18:40:14 -0500

    implemented getCategoryMarkupgetCategoryMarkup

--- src/MarkupCalculator.js
+++ src/MarkupCalculator.js
@@ -10,7 +10,25 @@ MarkupCalculator.prototype.calculatePerPersonMarkup = function(baseLineAndFlatMa
 };
 
 MarkupCalculator.prototype.getCategoryMarkup = function(category) {
-    
+   keywordsPharmaceuticals = ["drugs", "medicine"];
+   keywordsFood = ["food", "pizza"];
+   keywordsElectronics = ["ipads", "electronics"];
+   
+   output = 0;
+   
+   if(output == 0 && keywordsPharmaceuticals.indexOf(category) > -1){
+       output = 7.5;
+   }
+   
+   if(output == 0 && keywordsFood.indexOf(category) > -1){
+       output = 13;
+   }
+   
+   if(output == 0 && keywordsElectronics.indexOf(category) > -1){
+       output = 2;
+   }
+   
+   return output; 
 };
 
 MarkupCalculator.prototype.calculateCategoryMarkup = function(baseLineAndFlatMarkup, category) {
