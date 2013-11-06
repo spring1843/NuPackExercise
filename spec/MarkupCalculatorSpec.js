714afa798fbdf8e0b3d212459aaa10c50b86584f
Nicolas_Leonidas <yasser1984ir@gmail.com>
2013-11-06 17:46:04 -0500

    Added fake data, and calculate method to MarkupCalculator

--- spec/MarkupCalculatorSpec.js
+++ spec/MarkupCalculatorSpec.js
@@ -1,5 +1,11 @@
 describe("MarkupCalculator", function() {
 
+  var markupCalculator;
+
+  beforeEach(function() {
+    markupCalculator = new MarkupCalculator();
+  });
+
   it("There should be sanity", function() {
     expect(true).toEqual(true);
   });
--- src/MarkupCalculator.js
+++ src/MarkupCalculator.js
@@ -1,2 +1,6 @@
 function MarkupCalculator() {
 }
+
+MarkupCalculator.prototype.calculate = function(baseprice, people, category) {
+    throw new Error("not yet implemented");
+};
