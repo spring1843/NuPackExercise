118d21cce434c605a7dc7bca3fcd1c8d29e6da8d
Nicolas_Leonidas <yasser1984ir@gmail.com>
2013-11-06 17:56:15 -0500

    Added the first failing unit test

--- spec/MarkupCalculatorSpec.js
+++ spec/MarkupCalculatorSpec.js
@@ -1,13 +1,25 @@
 describe("MarkupCalculator", function() {
 
-  var markupCalculator;
+    var markupCalculator;
 
-  beforeEach(function() {
-    markupCalculator = new MarkupCalculator();
-  });
+    beforeEach(function() {
+        markupCalculator = new MarkupCalculator();
+    });
 
-  it("There should be sanity", function() {
-    expect(true).toEqual(true);
-  });
+    it("There should be sanity", function() {
+        expect(true).toEqual(true);
+    });
+  
+    it("Produces the correct output for each fake data", function() {
+    
+        for(i in fakeData){
+            baseprice = fakeData[i].baseprice;
+            people = fakeData[i].people;
+            category = fakeData[i].category;
+            output = fakeData[i].output;
+            expect(markupCalculator.calculate(baseprice, people, category)).toEqual(output);
+        }
+    
+    });
 
 });
--- src/MarkupCalculator.js
+++ src/MarkupCalculator.js
@@ -2,5 +2,5 @@ function MarkupCalculator() {
 }
 
 MarkupCalculator.prototype.calculate = function(baseprice, people, category) {
-    throw new Error("not yet implemented");
+    
 };
