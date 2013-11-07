46b5cecab6fb8b843f7a52fc61079013040bea87
Nicolas_Leonidas <yasser1984ir@gmail.com>
2013-11-06 22:40:44 -0500

    Clean up and refactoring round 1

--- spec/MarkupCalculatorSpec.js
+++ spec/MarkupCalculatorSpec.js
@@ -6,31 +6,31 @@ describe("MarkupCalculator", function() {
         markupCalculator = new MarkupCalculator();
     });
 
-    it("There should be sanity", function() {
+    it("There is sanity", function() {
         expect(true).toEqual(true);
     });
   
-    it("Calculates the flat markup", function() {
-        for(i in fakeData){
-            var baseprice = fakeData[i].baseprice;
+    it("Can calculate the flat markup", function() {
+        for(i in testData){
+            var baseprice = testData[i].baseprice;
             var flatMarkup =  baseprice * 5/100;
             expect(markupCalculator.calculateFlatMarkup(baseprice)).toEqual(flatMarkup);
         }
     });
     
-    it("Calculates per person markup", function() {
-        for(i in fakeData){
-            var  baseprice = fakeData[i].baseprice;
-            var  people = fakeData[i].people;
-            var   flatMarkup =  markupCalculator.calculateFlatMarkup(baseprice);
-            var   baseLineAndFlatMarkup = (flatMarkup + baseprice);
-            var   perPersonMarkup =  baseLineAndFlatMarkup * 1.2 * people/100;
+    it("Can calculate per person markup", function() {
+        for(i in testData){
+            var  baseprice = testData[i].baseprice;
+            var  people = testData[i].people;
+            var  flatMarkup =  markupCalculator.calculateFlatMarkup(baseprice);
+            var  baseLineAndFlatMarkup = (flatMarkup + baseprice);
+            var  perPersonMarkup =  baseLineAndFlatMarkup * 1.2 * people/100;
             expect(markupCalculator.calculatePerPersonMarkup(baseLineAndFlatMarkup, people)).toEqual(perPersonMarkup);
         }
     });
     
     
-    it("Gets the markup percentage based on category", function() {
+    it("Can get the markup percentage based on category", function() {
         expect(markupCalculator.getCategoryMarkup("food")).toEqual(13);
         expect(markupCalculator.getCategoryMarkup("drugs")).toEqual(7.5);
         expect(markupCalculator.getCategoryMarkup("electronics")).toEqual(2);
@@ -38,25 +38,25 @@ describe("MarkupCalculator", function() {
     });
     
     
-    it("Calculates markup for category", function() {
-        for(i in fakeData){
-            var   baseprice = fakeData[i].baseprice;
-            var   category = fakeData[i].category;
-            var   flatMarkup =  markupCalculator.calculateFlatMarkup(baseprice);
-            var   baseLineAndFlatMarkup = (flatMarkup + baseprice);
-            var   categoryMarkupPercentage = markupCalculator.getCategoryMarkup(category);
-            var   markupForCategory =  baseLineAndFlatMarkup * categoryMarkupPercentage/100;
+    it("Can calculate markup for category", function() {
+        for(i in testData){
+            var baseprice = testData[i].baseprice;
+            var category = testData[i].category;
+            var flatMarkup =  markupCalculator.calculateFlatMarkup(baseprice);
+            var baseLineAndFlatMarkup = (flatMarkup + baseprice);
+            var categoryMarkupPercentage = markupCalculator.getCategoryMarkup(category);
+            var markupForCategory =  baseLineAndFlatMarkup * categoryMarkupPercentage/100;
             expect(markupCalculator.calculateCategoryMarkup(baseLineAndFlatMarkup, categoryMarkupPercentage)).toEqual(markupForCategory);
         }
     });
     
     
-    it("Calculates the correct output for each fake data", function() {
-        for(i in fakeData){
-            var  baseprice = fakeData[i].baseprice;
-            var   people = fakeData[i].people;
-            var   category = fakeData[i].category;
-            var    output = fakeData[i].output;
+    it("Can calculate the correct output for each sample data", function() {
+        for(i in testData){
+            var baseprice = testData[i].baseprice;
+            var people = testData[i].people;
+            var category = testData[i].category;
+            var output = testData[i].output;
             expect(markupCalculator.calculate(baseprice, people, category)).toEqual(output);
         }
     
--- src/MarkupCalculator.js
+++ src/MarkupCalculator.js
@@ -1,46 +1,50 @@
 function MarkupCalculator() {
+    
+    // Constants
+    this.percentageFlat = 5;
+    this.percentagePerPerson = 1.2;
+    this.percentagePharmaceuticals = 7.5;
+    this.percentageFood = 13;
+    this.percentageElectronics = 2;
+    
+    
+    // Keywords related to each category
+    this.keywordsPharmaceuticals = ["drugs", "medicine"];
+    this.keywordsFood = ["food", "pizza"];
+    this.keywordsElectronics = ["electronics"];
+    
+    this.calculateFlatMarkup = function(baseprice) {
+        return baseprice * this.percentageFlat / 100;
+    };
+    
+    this.calculatePerPersonMarkup = function(baseLineAndFlatMarkup, people) {
+        return baseLineAndFlatMarkup * this.percentagePerPerson * people/100;
+    };
+    
+    this.getCategoryMarkup = function(category) {
+        if(this.keywordsPharmaceuticals.indexOf(category) > -1){
+            return this.percentagePharmaceuticals;
+        }
+        if(this.keywordsFood.indexOf(category) > -1){
+            return this.percentageFood;
+        }
+        if(this.keywordsElectronics.indexOf(category) > -1){
+            return this.percentageElectronics;
+        }
+        return 0;
+    };
+
+    this.calculateCategoryMarkup = function(baseLineAndFlatMarkup, categoryMarkupPercentage) {
+        return  baseLineAndFlatMarkup * categoryMarkupPercentage/100;
+    };
+    
+    this.calculate = function(baseprice, people, category) {
+        var flatMarkup = this.calculateFlatMarkup(baseprice);
+        var baseLineAndFlatMarkup = (flatMarkup + baseprice);
+        var perPersonMarkup = this.calculatePerPersonMarkup(baseLineAndFlatMarkup, people);
+        var categoryMarkupPercentage = this.getCategoryMarkup(category);
+        var markupForCategory = this.calculateCategoryMarkup(baseLineAndFlatMarkup, categoryMarkupPercentage)
+        var calculatedMarkup = parseFloat((baseLineAndFlatMarkup + perPersonMarkup + markupForCategory).toFixed(2));
+        return calculatedMarkup;
+    };
 }
-
-MarkupCalculator.prototype.calculateFlatMarkup = function(baseprice) {
-    return baseprice * 5 / 100;
-};
-
-MarkupCalculator.prototype.calculatePerPersonMarkup = function(baseLineAndFlatMarkup, people) {
-    return baseLineAndFlatMarkup * 1.2 * people/100;
-};
-
-MarkupCalculator.prototype.getCategoryMarkup = function(category) {
-    var keywordsPharmaceuticals = ["drugs", "medicine"];
-    var keywordsFood = ["food", "pizza"];
-    var keywordsElectronics = ["ipads", "electronics"];
-   
-    var output = 0;
-   
-    if(output == 0 && keywordsPharmaceuticals.indexOf(category) > -1){
-        output = 7.5;
-    }
-   
-    if(output == 0 && keywordsFood.indexOf(category) > -1){
-        output = 13;
-    }
-   
-    if(output == 0 && keywordsElectronics.indexOf(category) > -1){
-        output = 2;
-    }
-   
-    return output; 
-};
-
-MarkupCalculator.prototype.calculateCategoryMarkup = function(baseLineAndFlatMarkup, categoryMarkupPercentage) {
-    return  baseLineAndFlatMarkup * categoryMarkupPercentage/100;
-};
-
-MarkupCalculator.prototype.calculate = function(baseprice, people, category) {
-    var flatMarkup = this.calculateFlatMarkup(baseprice);
-var     baseLineAndFlatMarkup = (flatMarkup + baseprice);
-var     perPersonMarkup = this.calculatePerPersonMarkup(baseLineAndFlatMarkup, people);
-var     categoryMarkupPercentage = this.getCategoryMarkup(category);
-var     markupForCategory = this.calculateCategoryMarkup(baseLineAndFlatMarkup, categoryMarkupPercentage)
-var     calculatedMarkup = parseFloat((baseLineAndFlatMarkup + perPersonMarkup + markupForCategory).toFixed(2));
-    return calculatedMarkup;
-};
